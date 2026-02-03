7d2949da787c793c43e3f6974c154102d5157b6e
Esteban Amores <estebanamoresbarrantes@gmail.com>
2026-02-02 22:13:09 -0600

    NA

--- class_03/src/components/Store.js
+++ class_03/src/components/Store.js
@@ -2,7 +2,9 @@ import {combineReducers, legacy_createStore} from 'redux';
 import TaskReducer from './reducers/TaskReducer';
 
 const ConfigureStore = () => {
-    const reducers = combineReducers({Tasks: TaskReducer});
+    const reducers = combineReducers({
+        Tasks: TaskReducer
+    });
     const store = legacy_createStore(reducers);
     return store;
 };
